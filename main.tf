9e119957cd766ae8f502e6c47934a0644ea87ee7
mkawsgh <mkawsgh@gmail.com>
2025-04-17 18:55:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,16 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 2
-ami = "ami-01bd9d8f06d29d6a0"
+count = 4
+ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.micro"
+key_name = "devopsbyraham"
+vpc_security_group_ids = ["sg-0b8853497a3a91dc8"]
 tags = {
-Name = "mytfdemovm"
+Name = var.instance_names[count.index]
 }
 }
 
-resource "aws_s3_bucket" "one" {
-  bucket = "my-hcp-tfdemo-test-bucket183645"
+variable "instance_names" {
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
